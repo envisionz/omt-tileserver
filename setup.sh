53d5c15232f7a3ecfbc98bc6e97be0a2989565ea
shermp <14854761+shermp@users.noreply.github.com>
2021-09-20 21:55:32 +1200

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ compose_file_orig="${compose_file}.orig"
 
 printl()
 {
-    printf "%s\n" "$1"
+    printf "[OMT-Tileserver] %s\n" "$1"
 }
 
 restore_compose_file()
@@ -40,20 +40,34 @@ cd openmaptiles
 restore_compose_file
 
 cp "$compose_file" "$compose_file_orig"
-sed -i -e 's/pgdata:/pgdata:'"\\${nl}"'    external: true/' "$compose_file"
+sed -i -e 's/volumes:'"\\${nl}"'  pgdata:/volumes:'"\\${nl}"'  pgdata:'"\\${nl}"'    external: true/' "$compose_file"
 sed -i -e 's/pgdata:/omt-tileserver-pgdata:/g' "$compose_file"
 
-# Create 'omt-tileserver-pgdata' volume if it doesn't exist
-if ! docker volume ls | grep -q omt-tileserver-pgdata; then
-    docker volume create omt-tileserver-pgdata
-fi
+# Start with a clean slate
+printl "Running 'make clean'..."
+make clean
+printl "Destroying existing DB..."
+make destroy-db
 
+# Recreate 'omt-tileserver-pgdata' volume
+if docker volume ls | grep -q omt-tileserver-pgdata; then
+    docker volume rm omt-tileserver-pgdata
+fi
+docker volume create omt-tileserver-pgdata
+
+printl "Initialising..."
 make
+printl "Starting DB..."
 make start-db
+printl "Importing common data..."
 make import-data
+printl "Downloading area: ${dl_area}..."
 make download area="$dl_area"
+printl "Importing area: ${dl_area}..."
 make import-osm
+printl "Importing borders..."
 make import-borders
+printl "Importing SQL..."
 make import-sql
 
 restore_compose_file
