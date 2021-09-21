3c46814ab069de199a85dba478d10c87392b457e
Sherman Perry <sherman@envisionz.co.nz>
2021-09-21 15:54:19 +1200

    Start making tileserver more configurable

--- tileserver/entrypoint.sh
+++ tileserver/entrypoint.sh
@@ -9,23 +9,38 @@ printl()
 
 default_postserve_host="http://postserve:8080"
 
-postsrv_host="${POSTSERVE_HOST:-$default_postserve_host}"
+postsrv_host="${TILE_POSTSERVE_HOST:-$default_postserve_host}"
+
+domains=${TILE_DOMAINS:-'localhost:8181,127.0.0.1:8181'}
+
+min_zoom=${TILE_MIN_ZOOM:-0}
+max_zoom=${TILE_MAX_ZOOM:-20}
+bounds=${TILE_BOUNDS:-'-180,-85.0511,180,85.0511'}
+center=${TILE_CENTER:-'0,0,2'}
+attribution=${TILE_ATTR:-'<a href="https://openmaptiles.org/">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'}
+formats=${TILE_FORMAT:-'png'}
 
 cd /data/styles
 
-styles=(osm-bright-gl-style/style-local.json \
-    maptiler-basic-gl-style/style-local.json \
-    maptiler-terrain-gl-style/style-local.json \
-    dark-matter-gl-style/style-local.json \
-    positron-gl-style/style-local.json \
-    osm-liberty/style.json)
+declare -A styles
 
-for style in ${styles[@]}; do
-    cp "$style" /tmp/style.json
+styles[bright]=osm-bright-gl-style/style-local.json
+styles[basic]=maptiler-basic-gl-style/style-local.json
+styles[darkmatter]=dark-matter-gl-style/style-local.json
+styles[positron]=positron-gl-style/style-local.json
+styles[liberty]=osm-liberty/style.json
+
+tmp_config=/tmp/config.json
+
+for style in ${!styles[@]}; do
+    cp "${styles[$style]}" /tmp/style.json
     jq --arg p "$postsrv_host" \
         '.sources.openmaptiles.url = $p | .glyphs = "{fontstack}/{range}.pbf"' \
-        /tmp/style.json > "$style"
+        /tmp/style.json > "${styles[$style]}"
     rm /tmp/style.json
+    
+    cp /data/config.json "$tmp_config"
+    jq --arg sk "$style" --arg sf "${styles[$style]}" 
 done
 
 /app/docker-entrypoint.sh "$@"
