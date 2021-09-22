2ba4a408c80f8f6502087aa8f4ec4b3639042032
Sherman Perry <sherman@envisionz.co.nz>
2021-09-22 16:08:21 +1200

    Initial working tileserver configuration

--- tileserver/entrypoint.sh
+++ tileserver/entrypoint.sh
@@ -13,12 +13,16 @@ postsrv_host="${TILE_POSTSERVE_HOST:-$default_postserve_host}"
 
 domains=${TILE_DOMAINS:-'localhost:8181,127.0.0.1:8181'}
 
-min_zoom=${TILE_MIN_ZOOM:-0}
-max_zoom=${TILE_MAX_ZOOM:-20}
+# min_zoom=${TILE_MIN_ZOOM:-0}
+# max_zoom=${TILE_MAX_ZOOM:-20}
+
 bounds=${TILE_BOUNDS:-'-180,-85.0511,180,85.0511'}
+
 center=${TILE_CENTER:-'0,0,2'}
+
 attribution=${TILE_ATTR:-'<a href="https://openmaptiles.org/">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'}
-formats=${TILE_FORMAT:-'png'}
+
+format=${TILE_FORMAT:-png}
 
 cd /data/styles
 
@@ -30,17 +34,32 @@ styles[darkmatter]=dark-matter-gl-style/style-local.json
 styles[positron]=positron-gl-style/style-local.json
 styles[liberty]=osm-liberty/style.json
 
-tmp_config=/tmp/config.json
+tmp_style=".style.json.tmp"
+tmp_config=".config.json.tmp"
 
 for style in ${!styles[@]}; do
-    cp "${styles[$style]}" /tmp/style.json
+    cp "${styles[$style]}" "$tmp_style"
     jq --arg p "$postsrv_host" \
         '.sources.openmaptiles.url = $p | .glyphs = "{fontstack}/{range}.pbf"' \
-        /tmp/style.json > "${styles[$style]}"
-    rm /tmp/style.json
+        "$tmp_style" > "${styles[$style]}"
+    rm "$tmp_style"
     
     cp /data/config.json "$tmp_config"
-    jq --arg sk "$style" --arg sf "${styles[$style]}" 
+    jq --arg sk "$style" --arg sf "${styles[$style]}" --arg b "$bounds" --arg c "$center" --arg f "$format" --arg a "$attribution" \
+        '.styles += {($sk): {"style": $sf, "tilejson":{"format": $f, "attribution": $a, "bounds": [], "center": []}}} |
+         .styles[$sk].tilejson.bounds = ($b | split(",")) |
+         .styles[$sk].tilejson.bounds[] |= tonumber |
+         .styles[$sk].tilejson.center = ($c | split(",")) |
+         .styles[$sk].tilejson.center[] |= tonumber' \
+         "$tmp_config" > /data/config.json
+    rm "$tmp_config"
 done
+
+# Global config options
+cp /data/config.json "$tmp_config"
+jq --arg d "$domains" \
+    '.options.domains = ($d | split(","))' \
+    "$tmp_config" > /data/config.json
+rm "$tmp_config"
 
 /app/docker-entrypoint.sh "$@"
