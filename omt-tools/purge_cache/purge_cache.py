db4fffe458c04f51428adeb2efb7bef72307ca90
shermp <14854761+shermp@users.noreply.github.com>
2021-09-29 23:32:15 +1300

    Add more validation to TileMultiplier

--- omt-tools/purge_cache/purge_cache.py
+++ omt-tools/purge_cache/purge_cache.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 class TileMultiplier:
     """Generate unique list of map tiles from single map tiles
 
@@ -9,6 +11,8 @@ class TileMultiplier:
     Copyright (c) 2021 - Sherman Perry
     Copyright (c) 2019 - 2021 Makina Corpus
     """
+
+    tile_re = re.compile(r'[0-9]{1,2}/[0-9]+/[0-9]+')
     def __init__(self, min_zoom: int = 12, max_zoom: int = 20):
         """Initialise TileMultiplier
 
@@ -25,18 +29,33 @@ class TileMultiplier:
             tile = f'{z}/{x}/{y}'
             self.tile_set.add(tile)
     
+    def _validate_tile_str(self, tile: str) -> bool:
+        return TileMultiplier.tile_re.fullmatch(tile) != None
+    
+    def _validate_range(self, z: int, x: int, y: int) -> bool:
+        max = pow(2, z) - 1
+        return x <= max and y <= max
+    
     def multiply_tile(self, tile: str):
         """Multiply single tile to multiple tiles, and add to 'tile_set'
 
         Args:
             tile (str): tile in the form of 'z/x/y'
         """
+
+        if not self._validate_tile_str(tile):
+            return
+
         z, x, y = [int(i) for i in tile.split('/')]
         
         # Don't process tiles outside of the min-max zoom level range
         if z < self.min_zoom or z > self.max_zoom:
             return
-            
+        
+        # Also don't process tiles that have x and y coords out of range for their zoom level
+        if not self._validate_range(z, x, y):
+            return
+
         self._to_set(z, x, y)
 
         xx, yy = x, y
--- omt-tools/purge_cache/test_tile_multiplier.py
+++ omt-tools/purge_cache/test_tile_multiplier.py
@@ -1,7 +1,21 @@
 from purge_cache import TileMultiplier
 
-valid_tiles = {
-    '18/257820/162019': set()
+valid_tiles_set_len = {
+    '14/16117/10127': 5463
+}
+
+tile_format_str = {
+    '14/16117/10127': True,
+    '125/1234/5678': False,
+    'a/1234/5678': False,
+    'a/b/c': False,
+    '-3/4567/890': False,
+}
+
+ranges = {
+    (3, 9, 7): False,
+    (3, 6, 7): True,
+    (3, 5, 9): False,
 }
 
 def test_below_zoom():
@@ -13,3 +27,19 @@ def test_above_zoom():
     tm = TileMultiplier()
     tm.multiply_tile('24/128/128')
     assert len(tm.tile_set) == 0
+
+def test_valid_set_len():
+    for tile, set_len in valid_tiles_set_len.items():
+        tm = TileMultiplier()
+        tm.multiply_tile(tile)
+        assert len(tm.tile_set) == set_len
+
+def test_tile_str_format():
+    for fmt, valid in tile_format_str.items():
+        tm = TileMultiplier()
+        assert(tm._validate_tile_str(fmt) == valid)
+
+def test_range_validation():
+    for r, valid in ranges.items():
+        tm = TileMultiplier()
+        assert(tm._validate_range(r[0], r[1], r[2]) == valid)
