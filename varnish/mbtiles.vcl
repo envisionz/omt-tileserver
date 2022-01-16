9862b00410ef4f739ce130a29e8fdf971811c39d
shermp <14854761+shermp@users.noreply.github.com>
2022-01-16 13:47:55 +1300

    [varnish] support mbtiles, other improvements

---
+++ varnish/mbtiles.vcl
@@ -0,0 +1,60 @@
+# VCL script for varnish 4
+
+vcl 4.0;
+
+import std;
+
+backend default {
+  .host = "${tileserver_host}";
+  .port = "${tileserver_port}";
+  .probe = {
+    .url = "/health";
+    .timeout = 1s;
+    .interval = 5s;
+    .window = 5;
+    .threshold = 3;
+  }
+}
+
+sub vcl_recv {
+  unset req.http.cookie;
+
+  // Cache tiles and static images
+  if (req.url ~ "${tile_regex}") {
+    return (hash);
+  } elseif(req.url ~ "${static_regex}") {
+    return (hash);
+  } else {
+    return (pass);
+  }
+}
+
+sub vcl_backend_response {
+  // Remove all cookies
+  unset beresp.http.set-cookie;
+  unset beresp.http.cookie;
+
+  set beresp.grace = 2m;
+  set beresp.keep = 8m;
+
+  // set cache key based on tile coordinate, so all variants can be purged at once
+  // Also, provide minimal caching for high zoom levels
+  if (bereq.url ~ "${tile_regex}") {{
+    set beresp.ttl = 4w;
+  }
+  } elseif (bereq.url ~ "${static_regex}") {
+    set beresp.ttl = 4w;
+  }
+}
+
+sub vcl_hash {
+  // Cache using only path as a hash.  
+  // This means if a.tile/1/1/1/tile.png is accessed, b.tile/1/1/1/tile.png will also be fetch from cache
+  // Note, don't hash quey params for tiles
+  if (req.url ~ "${tile_regex}") {
+    hash_data(regsub(req.url, "^(.+${tile_regex}).*$", "\1"));
+  } else {
+    hash_data(req.url);
+  }
+  return (lookup);
+}
--- varnish/omt-varnish-entrypoint.sh
+++ varnish/omt-varnish-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mbtiles=${OMT_MBTILES}
+
 backend=${OMT_CACHE_STORAGE:-malloc}
 size=${OMT_CACHE_SIZE:-1G}
 
@@ -13,19 +15,55 @@ purge_host=${OMT_PURGE_HOST:-purge-cache}
 
 export OMT_CACHE_ZOOM_MAX=${OMT_CACHE_ZOOM_MAX:-18}
 
+# Cache regexes
+tile_regex='/[0-9]+/[0-9]+/[0-9]+(@[0-9]x)?\.(webp|png|jpg|jpeg|pbf)'
+zl_regex='^.+/([0-9]+)/[0-9]+/[0-9]+(@[0-9]x)?\.(webp|png|jpg|jpeg|pbf).*$'
+xkey_regex='^.+/([0-9]+/[0-9]+/[0-9]+)(@[0-9]x)?\.(webp|png|jpg|jpeg|pbf).*$'
+
+mvt_tile_regex='/tiles/[0-9]+/[0-9]+/[0-9]+\.pbf'
+static_regex='/styles/[a-zA-Z_\-]+/static/.+\.(webp|png|jpg|jpeg)'
+
 if [ "$backend" = "file" ]; then
     storage="file,/cache_store/store,${size}"
 else
     storage="malloc,${size}"
 fi
 
+if [ -n "$mbtiles" ]; then
+    cp /vcl_files/mbtiles.vcl /etc/varnish/default.vcl
+else
+    cp /vcl_files/postserve.vcl /etc/varnish/default.vcl
+    sed -i \
+        -e "s/\${postserve_host}/${postserve_host}/g" \
+        -e "s/\${postserve_port}/${postserve_port}/g" \
+        -e "s/\${purge_host}/${purge_host}/g" \
+        -e "s/\${zl_regex}/${zl_regex}/g" \
+        -e "s/\${xkey_regex}/${xkey_regex}/g" \
+        -e "s/\${mvt_tile_regex}/${mvt_tile_regex}/g" \
+        /etc/varnish/default.vcl || exit 1
+fi
 sed -i \
-    -e "s/\${postserve_host}/${postserve_host}/g" \
-    -e "s/\${postserve_port}/${postserve_port}/g" \
     -e "s/\${tileserver_host}/${tileserver_host}/g" \
     -e "s/\${tileserver_port}/${tileserver_port}/g" \
-    -e "s/\${purge_host}/${purge_host}/g" \
+    -e "s/\${tile_regex}/${tile_regex}/g" \
+    -e "s/\${static_regex}/${static_regex}/g" \
     /etc/varnish/default.vcl || exit 1
+
+if [ -z "$mbtiles" ]; then
+    # Varnish doesn't like if it can't resolve the purge host
+    # on startup.
+    until getent ahostsv4 "$purge_host"; do
+        echo "Error resolving cache purge hostname"
+        sleep 2
+    done
+fi
+
+# And to be on the safe side, ensure that the tileserver
+# is available
+until getent ahostsv4 "$tileserver_host"; do
+    echo "Error resolving cache purge hostname"
+    sleep 2
+done
 
 varnishd \
     -F \
